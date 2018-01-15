86e45bc83e72aef4c5bf84484e16e01d4ea9890d
Lahiru de Alwis <lahiru@zeptolytics.com>
2018-01-15 21:55:50 +0530

    new profile view

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -37,14 +37,14 @@
     @yield('content')
 
     <!-- jQuery -->
-    <script src="sb-admin/js/jquery.js"></script>
+    <script src="/sb-admin/js/jquery.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="sb-admin/js/bootstrap.min.js"></script>
+    <script src="/sb-admin/js/bootstrap.min.js"></script>
 
     <!-- Morris Charts JavaScript -->
-    <script src="sb-admin/js/plugins/morris/raphael.min.js"></script>
-    <script src="sb-admin/js/plugins/morris/morris.min.js"></script>
-    <script src="sb-admin/js/plugins/morris/morris-data.js"></script>
+    <script src="/sb-admin/js/plugins/morris/raphael.min.js"></script>
+    <script src="/sb-admin/js/plugins/morris/morris.min.js"></script>
+    <script src="/sb-admin/js/plugins/morris/morris-data.js"></script>
 </body>
 </html>
